2ba771a531b41b4455cdd60a36b2ef2d2cc2ea26
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-01-09 19:09:20 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Parth Antala on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
